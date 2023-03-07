483973f79a1c741e4a6d4ad3185dcaf93c4f38c1
jlzg1987 <jlzg1987@hotmail.com>
2023-03-07 16:59:53 -0500

    update

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -3,7 +3,7 @@ const { createUser } = require('../controlers/users.controlers');
 
 const router = Router();
 
-router.post('/api/v1/users',createUser);
+router.post('/api/v1/tasks',createUser);
 
 
 module.exports = router;
